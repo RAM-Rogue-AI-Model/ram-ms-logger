e89daeaf36094d9fc7261978a307d5a3a6bf42c6
Hugo CREPIN <crepin.hugo30@gmail.com>
2026-01-28 15:03:24 +0100

    init

---
+++ app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 3001; //TODO: use .env
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on port ${port}`);
+});
