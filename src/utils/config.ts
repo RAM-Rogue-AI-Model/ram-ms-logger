194c94989025e8f17dfa05e7215d58317bf27497
OlivierRecher <recher.olivier@outlook.fr>
2026-02-02 11:41:00 +0100

    feat(): env file with fake secret

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,14 +1,14 @@
 import { configType } from '../types/config';
 
 const config: configType = {
-  PORT: Number(process.env.PORT ?? 3001),
+  PORT: Number(process.env.PORT ?? 3007),
   DATABASE_URL: `mysql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${Number(process.env.DATABASE_PORT)}/${process.env.DATABASE_DB_NAME}`,
   DATABASE_HOST: process.env.DATABASE_HOST ?? 'localhost',
   DATABASE_USER: process.env.DATABASE_USER ?? 'user',
   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD ?? '',
-  DATABASE_PORT: Number(process.env.DATABASE_PORT ?? 3306),
+  DATABASE_PORT: Number(process.env.DATABASE_PORT ?? 3307),
   DATABASE_DB_NAME: process.env.DATABASE_DB_NAME ?? 'ram-ms-logger-db',
-    INTERNAL_SECRET: process.env.INTERNAL_SECRET ?? ""
+  INTERNAL_SECRET: process.env.INTERNAL_SECRET ?? ""
 };
 
 export { config };
